025f2c6c499d5384b0a55d172cb3bfd72c3b456a
Jannis R <mail@jannisr.de>
2016-12-22 11:40:29 +0100

    update to latest data, 1.1.0

--- download.sh
+++ download.sh
@@ -1,20 +1,4 @@
 #!/bin/sh
 
-URL='http://www.vbb.de/de/download/GTFS_VBB_Dez2016_Dez2017.zip'
-
-echo 'Downloading data from Berlin open data portal.'
-curl -# -L $URL > data.zip
-
-unzip -ju data.zip -d data
-rm data.zip
-rm 'data/GTFS_VBB_Dez2016 Dez2017_ohne_shape_files.zip'
-
-unzip -ju 'data/GTFS_VBB_Dez2016 Dez2017_mit_shape_files.zip' -d data
-rm 'data/GTFS_VBB_Dez2016 Dez2017_mit_shape_files.zip'
-
-rm data/agency.txt
-rm data/calendar.txt
-rm data/calendar_dates.txt
-rm data/stop_times.txt
-rm data/stops.txt
-rm data/transfers.txt
+curl -L --compressed https://vbb-gtfs.jannisr.de/latest/trips.txt > data/trips.txt
+curl -L --compressed https://vbb-gtfs.jannisr.de/latest/shapes.txt > data/shapes.txt
