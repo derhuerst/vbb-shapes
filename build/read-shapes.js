21616b8f5979665e7157e3f31479c4c62e6715fa
Jannis R <mail@jannisr.de>
2022-11-27 16:38:17 +0100

    Node 18+ 💥; build script: use gtfs-utils to read shapes.csv

--- build/read-shapes.js
+++ build/read-shapes.js
@@ -1,38 +1,32 @@
 'use strict'
 
+const readCsv = require('gtfs-utils/read-csv')
 const path = require('path')
-const fs = require('fs')
-const stripBOM = require('strip-bom-stream')
-const csv = require('csv-parser')
 
 const compress = require('../compress')
 
+const readFile = file => readCsv(path.join(__dirname, file + '.csv'))
 
-
-const readShapes = () => new Promise((yay, nay) => {
+const readShapes = async () => {
 	const shapes = Object.create(null)
 
-	fs.createReadStream(path.join(__dirname, 'shapes.csv'))
-	.pipe(stripBOM())
-	.pipe(csv())
-	.on('data', (row) => {
+	for await (const row of await readFile('shapes')) {
 		const data = [
 			parseFloat(row.shape_pt_lat), parseFloat(row.shape_pt_lon),
 			+row.shape_pt_sequence
 		]
 		if (!shapes[row.shape_id]) shapes[row.shape_id] = []
 		shapes[row.shape_id].push(data)
-	})
-	.on('end', () => {
-		for (let id in shapes) {
-			const shape = shapes[id]
-				.sort((a, b) => a[2] - b[2])
-				.map(([lat, lon, i]) => [lat, lon])
-			shapes[id] = compress(shape)
-		}
-		yay(shapes)
-	})
-	.on('error', nay)
-})
+	}
+
+	for (let id in shapes) {
+		const shape = shapes[id]
+			.sort((a, b) => a[2] - b[2])
+			.map(([lat, lon, i]) => [lat, lon])
+		shapes[id] = compress(shape)
+	}
+
+	return shapes
+}
 
 module.exports = readShapes
